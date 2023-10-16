4d05a457d6f6e38342748f80db3918a7a8e7bf73
jsw <csw9201@naver.com>
2023-10-16 13:16:16 +0900

    feat: todoform component

--- src/components/ToDoApp.tsx
+++ src/components/ToDoApp.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import { FormEvent, useState } from "react";
 
 const ToDoApp = () => {
+  const [value, setValue] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("할 일 추가", value);
+  };
   return (
     <div>
       <h1>To Do List</h1>
+
+      {/* form component */}
       <div>
-        <h2>form</h2>
-        <form>
+        <h2>Form</h2>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="할 일을 입력해 주세요."
+            onChange={(e) => setValue(e.currentTarget.value)}
+            value={value}
           />
           <button type="submit">추가</button>
         </form>
       </div>
+
+      {/* list component */}
       <div>
         <ul>
           <li>
